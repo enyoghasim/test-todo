b03ac2eccc0d8a5514b3e99b12fd10eddd035d38
David Enyoghasim <73113145+enyoghasim@users.noreply.github.com>
2025-06-10 17:33:20 +0100

    production b

--- src/app.ts
+++ src/app.ts
@@ -47,7 +47,7 @@ app.get("/health", (req: Request, res: Response) => {
   });
 });
 
-// Start serverel
+// Start serverel4
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`Health check: http://localhost:${PORT}/health`);
